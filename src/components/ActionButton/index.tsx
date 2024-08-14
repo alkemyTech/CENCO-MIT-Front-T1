ec16703cb7ba21bc5e7c84ab9e377776d55dfc4f
Bree2003 <breeale2003@gmail.com>
2024-08-14 00:35:16 -0400

    feat: created action button with edit and eliminate action

---
+++ src/components/ActionButton/index.tsx
@@ -0,0 +1,54 @@
+import { MoreVert } from "@mui/icons-material";
+import { Box, Popover } from "@mui/material";
+import React, { useState } from "react";
+import EditButton from "./EditButton";
+import DeleteButton from "./DeleteButton";
+
+interface ActionButtonProps {
+  userID: number;
+}
+
+export default function ActionButton({ userID }: Readonly<ActionButtonProps>) {
+  // popover
+  const [anchorEl, setAnchorEl] = useState<SVGSVGElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<SVGSVGElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  return (
+    <>
+      <MoreVert
+        aria-describedby={id}
+        onClick={handleClick}
+        sx={{ color: "var(--text-color)", cursor: "pointer" }}
+      />
+      <h1>{userID}</h1>
+
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <Box display={"block"} padding={1.5}>
+          <EditButton userID={userID} />
+          <DeleteButton userID={userID} />
+        </Box>
+      </Popover>
+    </>
+  );
+}
